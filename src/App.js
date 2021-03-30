bf5b81bd66c65a557a2905d2b1de6340670f5794
bakai <thedesport20@gmail.com>
2021-03-30 21:11:54 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="app">
+            <div className="app__sidebar">
+                asddsa
+            </div>
+
+            <div className="app__tasks"></div>
+        </div>
+    );
+}
+
+export default App;
