16eb7b106cb54a0779c7dec9f54ef196e069cff7
bakai <thedesport20@gmail.com>
2021-04-02 20:51:32 +0600

    added classnames library

--- src/App.js
+++ src/App.js
@@ -41,6 +41,18 @@ const App = () => {
                         name: "Фильмы и сериалы"
                     }
                 ]} />
+                <List items={[
+                    {
+                        id: 4,
+                        className: "list__add-button",
+                        icon: (<svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                        ),
+                        name: "Добавить список"
+                    }
+                ]} />
             </div>
 
             <div className="app__tasks"></div>
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import className from "classnames";
 
 import "./List.scss"
 
-const List = (props) => {
+const List = ({ items }) => {
     return (
         <ul className="list">
             {
-                props.items.map(item => (
-                    <li key={item.id} className={item.active ? "active" : ""}>
+                items.map(item => (
+                    <li key={item.id} className={className(item.className, {active: item.active})}>
                         <i>{item.icon
                             ? item.icon
                             : <i className={`badge badge__${item.color}`}></i>
