f383b3de67b9b3e5545eefa3bc74254d2b6962ec
bakai <thedesport20@gmail.com>
2021-04-12 19:04:33 +0600

    added toggle for visibility form

--- src/components/Tasks/AddTaskForm/index.jsx
+++ src/components/Tasks/AddTaskForm/index.jsx
@@ -5,15 +5,38 @@ import addSvg from "../../../assets/img/add.svg";
 import "./AddTaskForm.scss";
 
 const AddTaskForm = () => {
+    const [visibleForm, setVisibleForm] = React.useState(false);
+
+    const toggleVisibleForm = () => {
+        setVisibleForm(!visibleForm);
+    };
+
     return (
         <div className="tasks__form">
-            <div className="tasks__form-new">
-                <img src={addSvg} alt="add icon" />
-                <span>Новая задача</span>
-            </div>
-            <div>
-                
-            </div>
+            {
+                !visibleForm
+                    ? (
+                        <div onClick={toggleVisibleForm} className="tasks__form-new">
+                            <img src={addSvg} alt="add icon" />
+                            <span>Новая задача</span>
+                        </div>
+                    )
+                    : (
+                        <div className="tasks__form-block">
+                            <input
+                                className="field"
+                                type="text"
+                                placeholder="Текст задачи"
+                            />
+                            <button className="button">Добавить задачу</button>
+                            <button
+                                onClick={toggleVisibleForm}
+                                className="button button-grey">
+                                Отмена
+                            </button>
+                        </div>
+                    )
+            }
         </div>
     );
 };
